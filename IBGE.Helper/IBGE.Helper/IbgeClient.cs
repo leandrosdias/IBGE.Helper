b377a5b149450906ce121fd647e91e2585a30d02
leandrosdias <leandrosdias0@gmail.com>
2019-10-04 00:05:16 -0300

    Adição do pacote Newtonsoft
    Adição do pacote Newtonsoft

---
+++ IBGE.Helper/IBGE.Helper/IbgeClient.cs
@@ -0,0 +1,41 @@
+﻿using IBGE.Helper.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBGE.Helper
+{
+    public class IbgeClient
+    {
+        private HttpClient _httpClient;
+        public IbgeClient()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+            _httpClient = new HttpClient(handler);
+        }
+
+        public async Task<IEnumerable<Municipio>> GetMunicipiosAsync()
+        {
+           
+            var url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios"; ;
+
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
+            {
+                var response = await _httpClient.SendAsync(request);
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Municipio>>(jsonResponse);
+            }
+        }
+    }
+}
