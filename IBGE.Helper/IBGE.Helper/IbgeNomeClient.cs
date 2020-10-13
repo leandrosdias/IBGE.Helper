caeea3533bfe1a2c5e1763f8dd8066f75351a7eb
leandrosdias <leandrosdias0@gmail.com>
2020-10-13 11:37:56 -0300

    Criação da API de nomes

---
+++ IBGE.Helper/IBGE.Helper/IbgeNomeClient.cs
@@ -0,0 +1,38 @@
+﻿using IBGE.Helper.Models.Nomes;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IBGE.Helper
+{
+
+    public class IbgeNomeClient
+    {
+        private readonly HttpClient _httpClient;
+        public IbgeNomeClient()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+            _httpClient = new HttpClient(handler);
+        }
+
+        public async Task<IEnumerable<Nomes>> GetNamesAsync(List<string> names)
+        {
+            var url = $"https://servicodados.ibge.gov.br/api/v2/censos/nomes/" + string.Join("|", names);
+
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
+            {
+                var response = await _httpClient.SendAsync(request);
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Nomes>>(jsonResponse);
+            }
+        }
+    }
+}
---
+++ IBGE.Helper/IBGE.Helper/Models/Nomes/Nomes.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBGE.Helper.Models.Nomes
+{
+    public class Nomes
+    {
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+        [JsonProperty("sexo")]
+        public string Genero { get; set; }
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+        [JsonProperty("res")]
+        public Result[] Result { get; set; }
+    }
+
+    public class Result
+    {
+        [JsonProperty("periodo")]
+        public string Periodo { get; set; }
+        [JsonProperty("frequencia")]
+        public int Frequencia { get; set; }
+    }
+
+    public class NameFrequency
+    {
+        public string Name { get; set; }
+        public string Genero { get; set; }
+        public string Localidade { get; set; }
+        public int InitialYear { get; set; }
+        public int FinalYear { get; set; }
+        public int Frequency { get; set; }
+    }
+
+}
